82cdf0a6489308ddb242625ed8f3c20869eee607
CiaraMaria Proia <proiacm@gmail.com>
2020-03-24 17:28:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array =  [red, blue, green, yellow]
+  array =  [red, blue, green, yellow, orange]
 end
 
 def add_element_to_end_of_array(array, element)
