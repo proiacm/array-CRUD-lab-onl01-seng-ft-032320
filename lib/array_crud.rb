417700dae9e2b0d287f9015bfed9d5aa99d747d6
CiaraMaria Proia <proiacm@gmail.com>
2020-03-24 17:36:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
    color_array =  ["red", "blue", "green", "yellow"]
-   color_array
+   color_array.unshift
 end
 
 def remove_element_from_end_of_array(array)
