594b6204b9b2bf02da7d66558c79b9e42bd069d4
CiaraMaria Proia <proiacm@gmail.com>
2020-03-24 17:42:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  color_array =  ["arrays!","red", "blue", "green"]
+  color_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
